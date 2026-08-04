b75bcea221fd0b743264d35fbce97e9e2aa3d8f1
Ahmed-Osama99 <ahmed.osama.web@outlook.com>
2026-08-04 22:14:10 +0300

    end navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import HeroSection from './components/HeroSection.jsx'
 import Navbar from './components/Navbar.jsx'
 
 function App() {
   return (
     <>
       <Navbar />
+      <main>
+        <HeroSection/>
+      </main>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <header className=" container py-3 shadow flex items-center justify-between">
       <h1 className="ao-name font-caveat text-deep-teal text-2xl font-bold">Ahmed Osama</h1>
-      <button>
+      <button className="md:hidden">
         <FontAwesomeIcon
           icon={faBars}
           onClick={toggleNav}
@@ -19,7 +19,7 @@ const Navbar = () => {
       {/* mobile menu */}
       
         <nav className={`absolute p-4 z-10 top-0 left-0 right-0 w-full bg-deep-teal md:hidden transition-transform duration-200 ease-in-out transform ${isNavOpen ? "translate-y-0" : "-translate-y-full"}`}>
-          <button>
+          <button className="md:hidden">
             <FontAwesomeIcon
               icon={faXmark}
               onClick={toggleNav}
@@ -52,6 +52,18 @@ const Navbar = () => {
           onClick={toggleNav}
         ></div>
       )}
+      {/* desktop menu */}
+      <nav className="hidden md:flex items-center space-x-8">
+        <NavLink href="#portfolio" className="hover:text-muted-teal">
+          Projects
+        </NavLink>
+        <NavLink href="#about" className="hover:text-muted-teal">
+          Skills
+        </NavLink>
+        <NavLink href="#contact" className="rounded-full px-4 py-1 bg-muted-teal text-charcoal-blue hover:bg-deep-teal transition-colors duration-300 hover:text-ash-grey">
+          Contact Me
+        </NavLink>
+      </nav>
     </header>
   );
 };
