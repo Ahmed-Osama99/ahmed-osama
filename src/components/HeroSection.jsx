a9c552c9a4f6517ba5200333a146c0e293593a85
Ahmed-Osama99 <ahmed.osama.web@outlook.com>
2026-08-09 23:15:11 +0300

    build hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,11 @@
-
 const HeroSection = () => {
-    return(
-        <section></section>
-    )
-}
+  return (
+    <section className="h-[calc(100vh-76px)] flex items-center justify-center special-bg">
+        <div>
+            <div><span></span></div>
+        </div>
+    </section>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,15 @@
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
-
 const Navbar = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-  const toggleNav = () => setIsNavOpen(!isNavOpen);
   return (
-    <header className=" container py-3 shadow flex items-center justify-between">
-      <h1 className="ao-name font-caveat text-deep-teal text-2xl font-bold">Ahmed Osama</h1>
-      <button className="md:hidden">
-        <FontAwesomeIcon
-          icon={faBars}
-          onClick={toggleNav}
-          className="text-xl md:hidden text-deep-teal "
-        />
-      </button>
-      {/* mobile menu */}
-      
-        <nav className={`absolute p-4 z-10 top-0 left-0 right-0 w-full bg-deep-teal md:hidden transition-transform duration-200 ease-in-out transform ${isNavOpen ? "translate-y-0" : "-translate-y-full"}`}>
-          <button className="md:hidden">
-            <FontAwesomeIcon
-              icon={faXmark}
-              onClick={toggleNav}
-              className="text-xl absolute top-4 right-4"
-            />
-          </button>
-          <ul className=" flex flex-col justfiy-center space-y-8">
-            <li>
-              <NavLink href="#portfolio" className="hover:text-muted-teal">
-                Projects
-              </NavLink>
-            </li>
-            <li>
-              <NavLink href="#about" className="hover:text-muted-teal">
-                Skills
-              </NavLink>
-            </li>
-            <li>
-              <NavLink href="#contact" className="hover:text-muted-teal">
-                Contact Me
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      
-      {/* mobile overlay */}
-      {isNavOpen && (
-        <div
-          className="fixed inset-0 bg-[#222]/20 bg-opacity-50 transition-opacity duration-300 ease-in-out"
-          onClick={toggleNav}
-        ></div>
-      )}
-      {/* desktop menu */}
-      <nav className="hidden md:flex items-center space-x-8">
-        <NavLink href="#portfolio" className="hover:text-muted-teal">
-          Projects
-        </NavLink>
-        <NavLink href="#about" className="hover:text-muted-teal">
-          Skills
-        </NavLink>
-        <NavLink href="#contact" className="rounded-full px-4 py-1 bg-muted-teal text-charcoal-blue hover:bg-deep-teal transition-colors duration-300 hover:text-ash-grey">
-          Contact Me
-        </NavLink>
-      </nav>
+    <header className=" container py-4 flex items-center justify-between">
+      <h1 className="ao-name font-caveat text-black text-2xl font-bold">
+        Ahmed Osama
+      </h1>
+      <a
+        href="https://wa.me/201147480962"
+        className="bg-main px-4 py-2.5 rounded-full ring-4 hover:inset-shadow-black/70 hover:inset-shadow-sm transition-all ring-main/20 shadow uppercase text-white "
+      >
+        Contact
+      </a>
     </header>
   );
 };
